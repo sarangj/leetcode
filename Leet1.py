d37e7b19590202b00de37300803991bfd4046455
sarangj <sarangjoshi22@gmail.com>
2017-12-09 15:18:23 -0600

    [leet] Remember negative numbers

--- Leet1.py
+++ Leet1.py
@@ -1,10 +1,14 @@
 class Solution:
     def twoNum(self, nums, target): 
-
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         rmap = dict()
         for i, num in enumerate(nums):
             if num in rmap:
                 return [rmap[num], i]
-            if num <= target:
+            if abs(num) <= abs(target):
                 rmap[target - num] = i
         return
