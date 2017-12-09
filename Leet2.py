3e1b43e1713e59a2c94aa05071b7e85a2f355f1f
sarangj <sarangjoshi22@gmail.com>
2017-12-09 16:23:12 -0600

    [leet] Try to actually use data structures properly

--- Leet2.py
+++ Leet2.py
@@ -11,10 +11,22 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        return self.toInt(l1, 0, 0) + self.toInt(l2, 0, 0)
+        x = l1.val + l2.val
+        node = ListNode(x % 10)
+        self.computeNext(node, l1.next, l2.next, x / 10)
+        return node
 
-    def toInt(self, l, curr, i):
-        res = curr + (l.val * (10**i))
-        if l.next is None:
-            return res
-        return self.toInt(l.next, res, i + 1)
+    def computeNext(self, curr, l1, l2, carry):
+        v = l1.val + l2.val + carry
+        newCarry = v / 10
+        # assume l1 and l2 are the same length...
+        if l1.next is None:
+            if v > 9:
+                next = ListNode(newCarry)
+                next.next = ListNode(v / 10)
+                curr.next = next
+            curr.next = ListNode(v % 10)
+        next = ListNode(v % 10)
+        self.computeNext(next, l1.next, l2.next, newCarry)
+
+            
